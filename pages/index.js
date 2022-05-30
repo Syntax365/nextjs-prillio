91ef3bc9eba63859dbb4de441fb80336ee42ef81
Syntax365 <syntax365@gmail.com>
2022-05-30 19:20:26 -0400

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
-export default function Home() {
+function Homepage() {
   return <h1 className="text-3xl font-bold underline">Homepage</h1>;
 }
+
+export default Homepage;
--- pages/key-calculator/index.js
+++ pages/key-calculator/index.js
@@ -1,3 +1,5 @@
-export default function KeyCalculator() {
+function KeyCalculator() {
   return <h1 className="text-3xl font-bold underline">Key Calculator</h1>;
 }
+
+export default KeyCalculator;
